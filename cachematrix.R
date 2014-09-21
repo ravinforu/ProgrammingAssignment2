f1e657d62d92b43fcd88ffeed5c627fcf82070c7
Ravichandran Nagarajan <ravinforu@yahoo.com>
2014-09-21 22:51:15 +0530

    resolved the problem with variables and function names with respect to inverse

--- cachematrix.R
+++ cachematrix.R
@@ -10,11 +10,11 @@ makeCacheMatrix <- function(x = matrix()) {
     matx <<- NULL
   }
   get <- function() x
-  setmatrix <- function(solve) matx <<- solve
-  getmatrix <- function() matx
+  setmatrixinv <- function(solve) matx <<- solve
+  getmatrixinv <- function() matx
   list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+       setmatrixinv=setmatrixinv,
+       getmatrixinv=getmatrixinv)
 }
 
 
@@ -22,13 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  matinv <- x$getinverse()
+  matinv <- x$getmatrixinv()
   if(!is.null(matinv)){
     message("getting cached data")
     return(matinv)
   }
   matrix <- x$get()
   matinv <- solve(matrix, ...)
-  x$setmatrix(matinv)
+  x$setmatrixinv(matinv)
   matinv
 }
